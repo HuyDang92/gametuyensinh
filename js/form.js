1cf0825a41d7931e145f0195e26127d097336b46
HuyDang92 <95539752+HuyDang92@users.noreply.github.com>
2023-11-01 23:10:58 +0700

    first

---
+++ js/form.js
@@ -0,0 +1,40 @@
+const fullName = document.querySelector("#fullName");
+const age = document.querySelector("#age");
+const phone = document.querySelector("#phone");
+const submit = document.querySelector("#form");
+const message = document.querySelector("#message");
+
+const submitUser = () => {
+   if (fullName.value === "" || age.value === "" || phone.value === "") {
+      message.innerHTML = "Vui lòng nhập đầy đủ thông tin";
+      return;
+   }
+   const postData = {
+      name: fullName.value,
+      phone: phone.value,
+      age: age.value,
+   };
+   // Hiển thị hiệu ứng loading
+   document.getElementById("loading").style.display = "block";
+
+   axios
+      .post("https://gamepoly.onrender.com/api/v1/users/create", postData)
+      .then(function (response) {
+         // Xử lý dữ liệu từ phản hồi
+         const data = response.data;
+         console.log(data);
+         if (data?.status === "success") {
+            localStorage.setItem("currentPlay", 1);
+            document.getElementById("loading").style.display = "none";
+
+            window.location.href = "game.html";
+         } else {
+            alert("Xảy ra lỗi xin vui lòng thử lại!");
+         }
+      })
+      .catch(function (error) {
+         // Xử lý lỗi nếu có
+         console.error(error);
+      });
+};
+submit.addEventListener("submit", submitUser);
